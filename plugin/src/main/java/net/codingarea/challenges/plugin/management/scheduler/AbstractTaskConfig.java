coding-area-net/Challenges

commit 4d7b53cb714eadd570ff7a32bae75b3f09e30e69

implement lombok, fix typo

plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/ScheduledTaskConfig.java
@@ -1,15 +1,17 @@
 package net.codingarea.challenges.plugin.management.scheduler;
 
-import net.codingarea.challenges.plugin.management.scheduler.task.ScheduledTask;
-
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import net.codingarea.challenges.plugin.management.scheduler.task.ScheduledTask;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 2.0
  */
+@Getter
+@EqualsAndHashCode(callSuper = false)
 public final class ScheduledTaskConfig extends AbstractTaskConfig {
 
 	private final int rate;
@@ -21,23 +23,6 @@ public final class ScheduledTaskConfig extends AbstractTaskConfig {
 	ScheduledTaskConfig(@Nonnegative int rate, boolean async) {
 		super(async);
 		this.rate = rate;
-	}
-
-	public int getRate() {
-		return rate;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		ScheduledTaskConfig that = (ScheduledTaskConfig) o;
-		return rate == that.rate && async == that.async;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(rate, async);
 	}
 
 }

plugin/pom.xml
@@ -22,12 +22,6 @@
 
         <!-- Minecraft Dependencies -->
         <dependency>
-            <groupId>com.mojang</groupId>
-            <artifactId>authlib</artifactId>
-            <version>1.5.21</version>
-            <scope>provided</scope>
-        </dependency>
-        <dependency>
             <groupId>org.spigotmc</groupId>
             <artifactId>spigot-api</artifactId>
             <version>${spigot.version}</version>
@@ -37,6 +31,12 @@
             <groupId>com.github.anweisen.Utility</groupId>
             <artifactId>bukkit-utils</artifactId>
             <version>${utilities.version}</version>
+        </dependency>
+        <dependency>
+            <groupId>com.mojang</groupId>
+            <artifactId>authlib</artifactId>
+            <version>1.5.21</version>
+            <scope>provided</scope>
         </dependency>
 
         <!-- Cloud Dependencies -->
@@ -72,6 +72,12 @@
         </dependency>
 
         <!-- Annotations -->
+        <dependency>
+            <groupId>org.projectlombok</groupId>
+            <artifactId>lombok</artifactId>
+            <version>1.18.34</version>
+            <scope>provided</scope>
+        </dependency>
         <dependency>
             <groupId>com.google.code.findbugs</groupId>
             <artifactId>jsr305</artifactId>
@@ -125,6 +131,14 @@
                 <configuration>
                     <source>1.8</source>
                     <target>1.8</target>
+                    <encoding>utf-8</encoding>
+                    <annotationProcessorPaths>
+                        <path>
+                            <groupId>org.projectlombok</groupId>
+                            <artifactId>lombok</artifactId>
+                            <version>1.18.34</version>
+                        </path>
+                    </annotationProcessorPaths>
                 </configuration>
             </plugin>
 

plugin/src/main/java/net/codingarea/challenges/plugin/Challenges.java
@@ -1,5 +1,6 @@
 package net.codingarea.challenges.plugin;
 
+import lombok.Getter;
 import net.anweisen.utilities.bukkit.core.BukkitModule;
 import net.anweisen.utilities.common.version.Version;
 import net.codingarea.challenges.plugin.challenges.custom.settings.CustomSettingsLoader;
@@ -29,6 +30,7 @@ import javax.annotation.Nonnull;
  * @author KxmischesDomi | https://github.com/kxmischesdomi
  * @since 1.0
  */
+@Getter
 public final class Challenges extends BukkitModule {
 
 	private static Challenges instance;
@@ -48,10 +50,10 @@ public final class Challenges extends BukkitModule {
 	private WorldManager worldManager;
 	private TitleManager titleManager;
 	private MenuManager menuManager;
-	private ChallengeTimer timer;
+	private ChallengeTimer challengeTimer;
 	private LoaderRegistry loaderRegistry;
 	private MetricsLoader metricsLoader;
-	private GameWorldStorage gameWorldStorage;
+  private GameWorldStorage gameWorldStorage;
 	private GeneratorWorldPortalManager generatorWorldPortalManager;
 
 	@Nonnull
@@ -101,7 +103,7 @@ public final class Challenges extends BukkitModule {
 		scoreboardManager = new ScoreboardManager();
 		cloudSupportManager = new CloudSupportManager();
 		titleManager = new TitleManager();
-		timer = new ChallengeTimer();
+		challengeTimer = new ChallengeTimer();
 		blockDropManager = new BlockDropManager();
 		challengeManager = new ChallengeManager();
 		challengeLoader = new ChallengeLoader();
@@ -127,8 +129,8 @@ public final class Challenges extends BukkitModule {
 		customSettingsLoader.enable();
 		databaseManager.enable();
 		worldManager.enable();
-		timer.loadSession();
-		timer.enable();
+		challengeTimer.loadSession();
+		challengeTimer.enable();
 		challengeManager.enable();
 		statsManager.register();
 		scheduler.start();
@@ -196,7 +198,7 @@ public final class Challenges extends BukkitModule {
 		boolean restoreDefaultsOnReset = getConfigDocument().getBoolean("restore-defaults-on-reset");
 
 		if (playerInventoryManager != null) playerInventoryManager.handleDisable();
-		if (timer != null && !shutdownBecauseOfReset) timer.saveSession(false);
+		if (challengeTimer != null && !shutdownBecauseOfReset) challengeTimer.saveSession(false);
 		if (scheduler != null) scheduler.stop();
 		if (loaderRegistry != null) loaderRegistry.disable();
 		if (databaseManager != null) databaseManager.disconnectIfConnected();
@@ -215,104 +217,6 @@ public final class Challenges extends BukkitModule {
 			}
 			challengeManager.clearChallengeCache();
 		}
-	}
-
-	public GeneratorWorldPortalManager getGeneratorWorldManager() {
-		return generatorWorldPortalManager;
-	}
-
-	public GameWorldStorage getGameWorldStorage() {
-		return gameWorldStorage;
-	}
-
-	@Nonnull
-	public CustomSettingsLoader getCustomSettingsLoader() {
-		return customSettingsLoader;
-	}
-
-	@Nonnull
-	public CustomChallengesLoader getCustomChallengesLoader() {
-		return customChallengesLoader;
-	}
-
-	@Nonnull
-	public ChallengeManager getChallengeManager() {
-		return challengeManager;
-	}
-
-	@Nonnull
-	public MenuManager getMenuManager() {
-		return menuManager;
-	}
-
-	@Nonnull
-	public ChallengeTimer getChallengeTimer() {
-		return timer;
-	}
-
-	@Nonnull
-	public ServerManager getServerManager() {
-		return serverManager;
-	}
-
-	@Nonnull
-	public ScheduleManager getScheduler() {
-		return scheduler;
-	}
-
-	@Nonnull
-	public ConfigManager getConfigManager() {
-		return configManager;
-	}
-
-	@Nonnull
-	public PlayerInventoryManager getPlayerInventoryManager() {
-		return playerInventoryManager;
-	}
-
-	@Nonnull
-	public WorldManager getWorldManager() {
-		return worldManager;
-	}
-
-	@Nonnull
-	public DatabaseManager getDatabaseManager() {
-		return databaseManager;
-	}
-
-	@Nonnull
-	public StatsManager getStatsManager() {
-		return statsManager;
-	}
-
-	@Nonnull
-	public ScoreboardManager getScoreboardManager() {
-		return scoreboardManager;
-	}
-
-	@Nonnull
-	public CloudSupportManager getCloudSupportManager() {
-		return cloudSupportManager;
-	}
-
-	@Nonnull
-	public ChallengeLoader getChallengeLoader() {
-		return challengeLoader;
-	}
-
-	@Nonnull
-	public BlockDropManager getBlockDropManager() {
-		return blockDropManager;
-	}
-
-	@Nonnull
-	public TitleManager getTitleManager() {
-		return titleManager;
-	}
-
-	@Nonnull
-	public LoaderRegistry getLoaderRegistry() {
-		return loaderRegistry;
 	}
 
 }

plugin/src/main/java/net/codingarea/challenges/plugin/challenges/custom/CustomChallenge.java
@@ -1,5 +1,7 @@
 package net.codingarea.challenges.plugin.challenges.custom;
 
+import lombok.Getter;
+import lombok.ToString;
 import net.anweisen.utilities.common.config.Document;
 import net.codingarea.challenges.plugin.Challenges;
 import net.codingarea.challenges.plugin.challenges.custom.settings.ChallengeExecutionData;
@@ -26,6 +28,8 @@ import java.util.UUID;
  * @author KxmischesDomi | https://github.com/kxmischesdomi
  * @since 2.1.0
  */
+@Getter
+@ToString
 public class CustomChallenge extends Setting {
 
 	private final UUID uuid;
@@ -36,12 +40,12 @@ public class CustomChallenge extends Setting {
 	private ChallengeAction action;
 	private Map<String, String[]> subActions;
 
-	public CustomChallenge(MenuType menuType, UUID uuid, Material displayItem, String name, ChallengeTrigger trigger,
+	public CustomChallenge(MenuType menuType, UUID uuid, Material displayItem, String displayName, ChallengeTrigger trigger,
 						   Map<String, String[]> subTriggers, ChallengeAction action, Map<String, String[]> subActions) {
 		super(menuType);
 		this.uuid = uuid;
 		this.material = displayItem;
-		this.name = name;
+		this.name = displayName;
 		this.trigger = trigger;
 		this.subTriggers = subTriggers;
 		this.action = action;
@@ -177,20 +181,8 @@ public class CustomChallenge extends Setting {
 		this.subActions = subActions;
 	}
 
-	public ChallengeAction getAction() {
-		return action;
-	}
-
-	public ChallengeTrigger getTrigger() {
-		return trigger;
-	}
-
 	public UUID getUniqueId() {
 		return uuid;
-	}
-
-	public Material getMaterial() {
-		return material;
 	}
 
 	@Nonnull
@@ -202,27 +194,6 @@ public class CustomChallenge extends Setting {
 	@Override
 	public String getUniqueName() {
 		return uuid.toString();
-	}
-
-	public Map<String, String[]> getSubTriggers() {
-		return subTriggers;
-	}
-
-	public Map<String, String[]> getSubActions() {
-		return subActions;
-	}
-
-	@Override
-	public String toString() {
-		return "CustomChallenge{" +
-				"uuid=" + uuid +
-				", material=" + material +
-				", name='" + name + '\'' +
-				", trigger=" + trigger +
-				", subTriggers=" + subTriggers +
-				", action=" + action +
-				", subActions=" + subActions +
-				'}';
 	}
 
 }

plugin/src/main/java/net/codingarea/challenges/plugin/challenges/custom/settings/ChallengeExecutionData.java
@@ -1,5 +1,6 @@
 package net.codingarea.challenges.plugin.challenges.custom.settings;
 
+import lombok.Getter;
 import net.codingarea.challenges.plugin.ChallengeAPI;
 import net.codingarea.challenges.plugin.Challenges;
 import net.codingarea.challenges.plugin.challenges.custom.settings.action.impl.CancelEventAction;
@@ -20,10 +21,14 @@ import java.util.*;
  */
 public class ChallengeExecutionData {
 
+	@Getter
 	private final IChallengeTrigger trigger;
+	@Getter
 	private final Map<String, List<String>> triggerData;
+	@Getter
 	private Entity entity;
 	private Runnable cancelAction;
+	@Getter
 	private int timesExecuting;
 
 	public ChallengeExecutionData(
@@ -88,22 +93,6 @@ public class ChallengeExecutionData {
 				cancelAction.run();
 			}
 		}
-	}
-
-	public IChallengeTrigger getTrigger() {
-		return trigger;
-	}
-
-	public Map<String, List<String>> getTriggerData() {
-		return triggerData;
-	}
-
-	public Entity getEntity() {
-		return entity;
-	}
-
-	public int getTimesExecuting() {
-		return timesExecuting;
 	}
 
 }

plugin/src/main/java/net/codingarea/challenges/plugin/challenges/custom/settings/CustomSettingsLoader.java
@@ -1,5 +1,6 @@
 package net.codingarea.challenges.plugin.challenges.custom.settings;
 
+import lombok.Getter;
 import net.anweisen.utilities.bukkit.utils.logging.Logger;
 import net.anweisen.utilities.bukkit.utils.misc.MinecraftVersion;
 import net.codingarea.challenges.plugin.Challenges;
@@ -20,9 +21,11 @@ import java.util.Map;
  */
 public class CustomSettingsLoader {
 
+	@Getter
 	private final Map<String, ChallengeTrigger> triggers;
 	private final Map<String, ChallengeTrigger> fallbackTriggers;
 
+	@Getter
 	private final Map<String, ChallengeAction> actions;
 	private final Map<String, ChallengeAction> fallbackActions;
 
@@ -155,14 +158,6 @@ public class CustomSettingsLoader {
 	@Nullable
 	public ChallengeTrigger getTriggerByName(String name) {
 		return triggers.getOrDefault(name, fallbackTriggers.get(name));
-	}
-
-	public Map<String, ChallengeAction> getActions() {
-		return actions;
-	}
-
-	public Map<String, ChallengeTrigger> getTriggers() {
-		return triggers;
 	}
 
 }

plugin/src/main/java/net/codingarea/challenges/plugin/challenges/custom/settings/action/impl/RandomMobAction.java
@@ -1,5 +1,6 @@
 package net.codingarea.challenges.plugin.challenges.custom.settings.action.impl;
 
+import lombok.Getter;
 import net.codingarea.challenges.plugin.challenges.custom.settings.action.ChallengeAction;
 import net.codingarea.challenges.plugin.challenges.custom.settings.action.EntityTargetAction;
 import net.codingarea.challenges.plugin.challenges.type.helper.SubSettingsHelper;
@@ -19,13 +20,15 @@ import java.util.stream.Collectors;
  */
 public class RandomMobAction extends EntityTargetAction {
 
-	private static final EntityType[] spannableMobs;
+	@Getter
+	private static final EntityType[] spawnableMobs;
+	@Getter
 	private static final EntityType[] livingMobs;
 
 	static {
 		List<EntityType> list = new LinkedList<>(Arrays.asList(EntityType.values()));
 		list = list.stream().filter(EntityType::isSpawnable).collect(Collectors.toList());
-		spannableMobs = list.toArray(new EntityType[0]);
+		spawnableMobs = list.toArray(new EntityType[0]);
 		list = list.stream().filter(EntityType::isAlive).collect(Collectors.toList());
 		livingMobs = list.toArray(new EntityType[0]);
 	}
@@ -34,18 +37,10 @@ public class RandomMobAction extends EntityTargetAction {
 		super(name, SubSettingsHelper.createEntityTargetSettingsBuilder(false));
 	}
 
-	public static EntityType[] getSpawnableMobs() {
-		return spannableMobs;
-	}
-
-	public static EntityType[] getLivingMobs() {
-		return livingMobs;
-	}
-
 	@Override
 	public void executeFor(Entity entity, Map<String, String[]> subActions) {
 		if (entity.getLocation().getWorld() == null) return;
-		EntityType value = ChallengeAction.random.choose(spannableMobs);
+		EntityType value = ChallengeAction.random.choose(spawnableMobs);
 		entity.getLocation().getWorld().spawnEntity(entity.getLocation(), value);
 	}
 

plugin/src/main/java/net/codingarea/challenges/plugin/challenges/custom/settings/sub/ValueSetting.java
@@ -1,5 +1,6 @@
 package net.codingarea.challenges.plugin.challenges.custom.settings.sub;
 
+import lombok.Getter;
 import net.anweisen.utilities.bukkit.utils.menu.MenuClickInfo;
 import net.codingarea.challenges.plugin.utils.item.ItemBuilder;
 
@@ -9,6 +10,7 @@ import net.codingarea.challenges.plugin.utils.item.ItemBuilder;
  */
 public abstract class ValueSetting {
 
+	@Getter
 	private final String key;
 	private final ItemBuilder itemBuilder;
 
@@ -28,9 +30,5 @@ public abstract class ValueSetting {
 	}
 
 	public abstract ItemBuilder getSettingsItem(String value);
-
-	public String getKey() {
-		return key;
-	}
 
 }

commit 04a4506ded80d70fe9ed3b79c49c0987cbd8fcfa

fix: scheduler synchronization issue introduced by 4d7b53c

plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/AbstractTaskConfig.java
@@ -1,8 +1,14 @@
 package net.codingarea.challenges.plugin.management.scheduler;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+// EqualsAndHashCode over `async` is intentional and load-bearing: subclass configs are used as
+// map keys to look up the shared task executor. If two configs at the same rate but different
+// thread affinity (sync vs async) compared equal, a sync task could be dispatched onto the async
+// executor (or vice versa). See ScheduledTaskConfig#callSuper.
 @Getter
+@EqualsAndHashCode
 public abstract class AbstractTaskConfig {
 
   protected final boolean async;

plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/ScheduledTaskConfig.java
@@ -6,7 +6,7 @@ import net.codingarea.challenges.plugin.management.scheduler.task.ScheduledTask;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper = true) // MUST include super.async - see AbstractTaskConfig
 public final class ScheduledTaskConfig extends AbstractTaskConfig {
 
   private final int rate;

plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/TimerTaskConfig.java
@@ -1,11 +1,13 @@
 package net.codingarea.challenges.plugin.management.scheduler;
 
+import lombok.EqualsAndHashCode;
 import net.codingarea.challenges.plugin.management.scheduler.task.TimerTask;
 import net.codingarea.challenges.plugin.management.scheduler.timer.TimerStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
+@EqualsAndHashCode(callSuper = true) // MUST include super.async - see AbstractTaskConfig
 public final class TimerTaskConfig extends AbstractTaskConfig {
 
   private final TimerStatus[] status;